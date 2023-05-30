efaba440a60629ed3e48077c47649b5b335cbf5d
Julia Sena <juliasenas@gmail.com>
2023-05-30 10:54:05 -0300

    Update Resultados.jsx

--- src/components/pages/Resultados.jsx
+++ src/components/pages/Resultados.jsx
@@ -1,27 +1,6 @@
 /* eslint jsx-a11y/anchor-is-valid: 0 */
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../styles/resultados.css";
-import acbrasil from "../img/cards/acbrazil.jpeg";
-import acium from "../img/cards/acium.jpg";
-import baciodilatte from "../img/cards/baciodilatte.jpg";
-import boticario from "../img/cards/boticario.jpg";
-import botoclinic from "../img/cards/botoclinic.jpg";
-import brasilcacau from "../img/cards/brasilcacau.jpg";
-import cabana from "../img/cards/cabana.png";
-import cantao from "../img/cards/cantao.jpg";
-import chillibeans from "../img/cards/chillibeans.jpg";
-import gendai from "../img/cards/gendai.jpg";
-import kfc from "../img/cards/kfc.jpg";
-import mammajamma from "../img/cards/mammajamma.webp";
-import oakberry from "../img/cards/oakberry.jpg";
-import peahi from "../img/cards/peahi.jpg";
-import polishop from "../img/cards/polishop.jpg";
-import rommanel from "../img/cards/rommanel.jpg";
-import starbucks from "../img/cards/starbucks.jpg";
-import thecoffee from "../img/cards/thecoffee.jpg";
-import tokstok from "../img/cards/tokstok.jpg";
-import propet from "../img/cards/propet.jpg";
-import uza from "../img/cards/uza.jpg";
 import Footer from "../home/Footer";
 import Navbar from "../home/Nav";
 
@@ -35,94 +14,148 @@ export default function Resultados() {
       valor: "R$ 15.000/mês",
     },
     {
-      imgSrc: "../img/cards/acium.jpg",
+      imgSrc: require("../img/cards/acium.jpg"),
       title: "Acium",
       estado: "São Paulo",
       descricao: "Quiosque. 10m²",
       valor: "R$ 4.000/mês",
     },
     {
-      imgSrc: "../img/cards/baciodilatte.jpg",
+      imgSrc: require("../img/cards/baciodilatte.jpg"),
       title: "Bacio di Latte",
       estado: "Rio de Janeiro",
       descricao: "Restaurante. 50m²",
       valor: "R$ 12.000/mês",
     },
     {
-      imgSrc: "../img/cards/boticario.jpg",
+      imgSrc: require("../img/cards/boticario.jpg"),
       title: "O Boticário",
       estado: "Rio de Janeiro",
       descricao: "Quiosque. 15m²",
       valor: "R$ 6.000/mês",
     },
     {
-      imgSrc: "../img/cards/botoclinic.jpg",
+      imgSrc: require("../img/cards/botoclinic.jpg"),
       title: "Botoclinic",
       estado: "Rio de Janeiro",
       descricao: "Loja de shopping. 150m²",
       valor: "R$ 20.000/mês",
     },
     {
-      imgSrc: "../img/cards/brasilcacau.jpg",
+      imgSrc: require("../img/cards/brasilcacau.jpg"),
       title: "Chocolates Brasil Cacau",
       estado: "São Paulo",
       descricao: "Quiosque. 12m²",
       valor: "R$ 6.000/mês",
     },
     {
-      imgSrc: "../img/cards/cabana.png",
+      imgSrc: require("../img/cards/cabana.png"),
       title: "Cabana Burger",
       estado: "São Paulo",
       descricao: "Restaurante. 70m²",
       valor: "R$ 16.000/mês",
     },
     {
-      imgSrc: "../img/cards/cantao.jpg",
+      imgSrc: require("../img/cards/cantao.jpg"),
       title: "Cantao",
       estado: "Rio de Janeiro",
       descricao: "Loja de shopping. 65m²",
       valor: "R$ 14.000/mês",
     },
     {
-      imgSrc: "../img/cards/chillibeans.jpg",
+      imgSrc: require("../img/cards/chillibeans.jpg"),
       title: "Chilli Beans",
       estado: "São Paulo",
       descricao: "Quiosque. 18m²",
       valor: "R$ 7.000/mês",
     },
     {
-      imgSrc: "../img/cards/gendai.jpg",
+      imgSrc: require("../img/cards/gendai.jpg"),
       title: "Gendai",
       estado: "São Paulo",
       descricao: "Restaurante. 160m²",
       valor: "R$ 18.000/mês",
     },
     {
-      imgSrc: "../img/cards/kfc.jpg",
+      imgSrc: require("../img/cards/kfc.jpg"),
       title: "KFC",
       estado: "São Paulo",
       descricao: "Restaurante. 55m²",
       valor: "R$ 11.000/mês",
     },
     {
-      imgSrc: "../img/cards/mammajamma.webp",
+      imgSrc: require("../img/cards/mammajamma.webp"),
       title: "Mamma Jamma",
       estado: "Rio de Janeiro",
       descricao: "Restaurante. 180m²",
       valor: "R$ 19.000/mês",
     },
     {
-    imgSrc: "../img/cards/oakberry.jpg",
+    imgSrc: require("../img/cards/oakberry.jpg"),
       title: "Oakberry",
       estado: "Rio de Janeiro",
+      descricao: "Quiosque. 14m²",
+      valor: "R$ 6.000/mês",
+    }
+    ,
+    {
+    imgSrc: require("../img/cards/peahi.jpg"),
+      title: "Peahi",
+      estado: "São Paulo",
+      descricao: "Loja de Shopping. 100m²",
+      valor: "R$ 13.000/mês",
+    },
+    {
+    imgSrc: require("../img/cards/polishop.jpg"),
+      title: "Polishop",
+      estado: "São Paulo",
+      descricao: "Quiosque. 14m²",
+      valor: "R$ 8.000/mês",
+    },
+    {
+    imgSrc: require("../img/cards/rommanel.jpg"),
+      title: "Rommanel",
+      estado: "São Paulo",
+      descricao: "Loja de Shopping. 70m²",
+      valor: "R$ 13.000/mês",
+    },
+    {
+    imgSrc: require("../img/cards/starbucks.jpg"),
+      title: "Starbucks",
+      estado: "Rio de Janeiro",
+      descricao: "Restaurante. 120m²",
+      valor: "R$ 27.000/mês",
+    },
+    {
+    imgSrc: require("../img/cards/thecoffee.jpg"),
+      title: "The Coffee",
+      estado: "São Paulo",
+      descricao: "Box. 10m²",
+      valor: "R$ 3.000/mês",
+    },
+    {
+    imgSrc: require("../img/cards/tokstok.jpg"),
+      title: "Tok Stok",
+      estado: "São Paulo",
+      descricao: "Loja de Shopping. 250m²",
+      valor: "R$ 45.000/mês",
+    },
+    {
+    imgSrc: require("../img/cards/propet.jpg"),
+      title: "ProPet",
+      estado: "Rio de Janeiro",
+      descricao: "Box. 12m²",
+      valor: "R$ 5.000/mês",
+    },
+    {
+    imgSrc: require("../img/cards/uza.jpg"),
+      title: "Uza",
+      estado: "São Paulo",
+      descricao: "Loja de Shopping. 70m²",
+      valor: "R$ 10.000/mês",
     }
   ];
 
-  useEffect(() => {
-  }, []);
-  
-
-    
   return (
     <div>
       <Navbar />
@@ -159,9 +192,7 @@ export default function Resultados() {
                         <p className="card-text">
                           <small className="text-muted">{card.valor}</small>
                         </p>
-                          <a href="#" className="btn btn-primary">
-                          Ver detalhes
-                        </a>
+       
                       </div>
                     </div>
                   </div>
