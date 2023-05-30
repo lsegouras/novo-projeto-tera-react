98259d7f9f3dc16cddc360cee12ffd68995bbd22
Julia Sena <juliasenas@gmail.com>
2023-05-30 14:04:16 -0300

    script dos resultados // pronto

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import mainPhoto from "../img/index/main.jpg";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  const navigate = useNavigate();
   const [tipo, setTipo] = useState("");
   const [estado, setEstado] = useState("");
   const [valor, setValor] = useState("");
   const [tamanho, setTamanho] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,45 +18,77 @@ const Header = () => {
       tamanho,
     });
 
-    navigate(`/resultados?${queryParams.toString()}`);
+    navigate(`/resultados?${queryParams}`);
   };
+
+  const handleTipoChange = (e) => {
+    setTipo(e.target.value);
+  };
+
+  const handleEstadoChange = (e) => {
+    setEstado(e.target.value);
+  };
+
+  const handleValorChange = (e) => {
+    const selectedValue = e.target.value;
+    setValor(selectedValue);
+  };
+
+  const handleTamanhoChange = (e) => {
+    const selectedValue = e.target.value;
+    setTamanho(selectedValue);
+  };
+
   return (
     <div className="header-content">
       <div className="left">
         <h1 className="form-title">Encontre a loja dos seus sonhos!</h1>
         <form className="formulario" onSubmit={handleSubmit}>
-          {" "}
           <label htmlFor="tipo">Tipo:</label>
-          <select name="tipo" id="tipo">
+          <select name="tipo" onChange={handleTipoChange}>
             <option value="">Todos</option>
-            <option value="Quiosque">Quiosque</option>
-            <option value="Restaurante">Restaurante</option>
-            <option value="Loja">Loja de Shopping</option>
-            <option value="Box">Box</option>
+            <option value="loja">Loja</option>
+            <option value="quiosque">Quiosque</option>
+            <option value="restaurante">Restaurante</option>
+            <option value="box">Box</option>
           </select>
+
           <label htmlFor="estado">Estado:</label>
-          <select name="estado" id="estado">
+          <select
+            name="estado"
+            id="estado"
+            value={estado}
+            onChange={handleEstadoChange}
+          >
             <option value="">Todos</option>
             <option value="Rio de Janeiro">Rio de Janeiro</option>
-            <option value="Sao Paulo">São Paulo</option>
+            <option value="São Paulo">São Paulo</option>
           </select>
           <label htmlFor="valor">Valor:</label>
-          <select name="valor" id="valor">
+          <select
+            name="valor"
+            id="valor"
+            value={valor}
+            onChange={handleValorChange}
+          >
             <option value="">Todos</option>
             <option value="ate8000">Até R$8.000/mês</option>
             <option value="8000a15000">De R$8.000 a R$15.000/mês</option>
             <option value="acima15000">Acima de R$15.000/mês</option>
           </select>
           <label htmlFor="tamanho">Tamanho:</label>
-          <select name="tamanho" id="tamanho">
+          <select
+            name="tamanho"
+            id="tamanho"
+            value={tamanho}
+            onChange={handleTamanhoChange}
+          >
             <option value="">Todos</option>
             <option value="ate50">Até 50m²</option>
             <option value="50a100">De 50m² a 100m²</option>
             <option value="100a200">De 100m² a 200m²</option>
           </select>
-          <Link to="/resultados">
-            <button type="submit">Filtrar</button>
-          </Link>
+          <button type="submit">Filtrar</button>
         </form>
       </div>
       <div className="right">
